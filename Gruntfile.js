07ed718f82b98634e3e2f439d42f47bf5cf55f8d
Taylor Fuqua <tfuqua@Franks-Mac.local>
2015-11-10 18:22:17 -0500

    updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,16 @@
 module.exports = function(grunt) {
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
   grunt.initConfig({
 
     // JS TASKS ================================================================
@@ -23,6 +34,17 @@ module.exports = function(grunt) {
       build: {
         files: {
           'public/dist/css/style.css': 'public/src/css/style.less'
+        }
+      }
+    },
+
+    sass: {
+      dist: {
+        options:{
+          style: 'compressed'
+        },
+        files: {
+          'public/dist/css/sass.css' : 'public/src/css/sass.scss'
         }
       }
     },
@@ -62,15 +84,6 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-nodemon');
-  grunt.loadNpmTasks('grunt-concurrent');
-
-  grunt.registerTask('default', ['less', 'cssmin', 'jshint', 'uglify', 'concurrent']);
+  grunt.registerTask('default', ['less', 'sass', 'cssmin', 'jshint', 'uglify', 'concurrent']);
 
 };
