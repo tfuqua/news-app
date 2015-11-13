8b771b3bb9464e21fa49a0dee79a0a502f458c2e
Taylor Fuqua <tfuqua@Franks-Mac.home>
2015-11-13 13:59:13 -0500

    beforeinstall script

--- deploy_scripts/BeforeInstall.sh
+++ deploy_scripts/BeforeInstall.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-pushd .
-sudo rm -rf /home/ec2-user/apps/news/*
-#need the pushd and popd because codedeploy with cd
-#will break the wholeagent
-popd
+
+rm -rf /home/ec2-user/apps/news
+mkdir /home/ec2-user/apps/news
